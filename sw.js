561eed8c8dd5761010ee559cae9144045a40d460
jamesursu-maker <james.ursu@gmail.com>
2026-09-08 12:56:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-const CACHE_NAME = 'assistant-manager-task-manager-v2';
+const CACHE_NAME = 'assistant-manager-task-manager-v16';
 
-const FILES_TO_CACHE = [
+const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .catch(() => {})
   );
+
   self.skipWaiting();
 });
 
@@ -23,17 +28,22 @@ self.addEventListener('activate', event => {
       )
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
+
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, copy);
         });
+
         return response;
       })
       .catch(() => caches.match(event.request))
